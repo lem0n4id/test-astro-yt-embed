0ab4d66f82f60e12209e4fd8b6a40146a531e912
lem0n4id <leninkennedy4work@gmail.com>
2023-06-15 20:10:54 +0530

    feat: Deploy to GitHub Pages
    followed the instructions at Astro docs to deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), tailwind()],
+	site: 'https://lem0n4id.github.io',
+  	base: '/test-astro-yt-embed',
 });
